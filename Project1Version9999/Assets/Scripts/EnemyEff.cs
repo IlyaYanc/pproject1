0d7db7f0cf0bb64b709ba2cb80d08d18589c137e
Itclub21 <itclubunitylab21@gmail.com>
2022-03-17 19:48:00 +0300

    Eff

---
+++ Project1Version9999/Assets/Scripts/EnemyEff.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyEff : MonoBehaviour
+{
+[SerializeField]
+private AudioSource AudS;
+   public void PlayEff()
+{
+AudS.Play();
+}
+}
