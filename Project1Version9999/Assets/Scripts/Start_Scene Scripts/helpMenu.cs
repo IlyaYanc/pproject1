1eb1a1bc490faae5c1285ff5d12c384f1780e6f5
Ilya_Yan <ilyayan1755@gmail.com>
2022-04-03 19:18:09 +0300

    scenes, prefabs, tiles

--- Project1Version9999/Assets/Scripts/Start_Scene Scripts/helpMenu.cs
+++ Project1Version9999/Assets/Scripts/Start_Scene Scripts/helpMenu.cs
@@ -2,95 +2,120 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class helpMenu : MonoBehaviour
 {
     [SerializeField]
-    private GameObject MP, IP, BP, AP, LP, SM;
+    private GameObject MP, IP, BP, AP, LP, SM, TrainingPanel;
     [SerializeField]
-    private GameObject MB, IB, BB, AB, LB;
+    private GameObject MB, IB, BB, AB, LB, TrainingButton, exitButton;
     private Color32 ActiveColor;
     private Color32 InactiveColor;
-
+    private GameObject ActivePanel, ActiveButton;
     private void Start()
     {
         ActiveColor = new Color32(171, 142, 77, 255);
         InactiveColor = new Color32(75, 75, 75, 255);
+        ActivePanel = MP;
+        ActiveButton = MB;
     }
+
     public void Move_button()
     {
-        MP.SetActive(true);
-        IP.SetActive(false);
-        BP.SetActive(false);
-        AP.SetActive(false);
-        LP.SetActive(false);
-
-        MB.GetComponent<Image>().color = ActiveColor;
-        IB.GetComponent<Image>().color = InactiveColor;
-        BB.GetComponent<Image>().color = InactiveColor;
-        AB.GetComponent<Image>().color = InactiveColor;
-        LB.GetComponent<Image>().color = InactiveColor;
+        DeactivatePanels(MP);
+        DeactivateButtons(MB);
     }
+
     public void Inter_button()
     {
-        IP.SetActive(true);
-        MP.SetActive(false);
-        BP.SetActive(false);
-        AP.SetActive(false);
-        LP.SetActive(false);
-
-        MB.GetComponent<Image>().color = InactiveColor;
-        IB.GetComponent<Image>().color = ActiveColor;
-        BB.GetComponent<Image>().color = InactiveColor;
-        AB.GetComponent<Image>().color = InactiveColor;
-        LB.GetComponent<Image>().color = InactiveColor;
+        DeactivatePanels(IP);
+        DeactivateButtons(IB);
     }
+
     public void Battel_button()
     {
-        BP.SetActive(true);
-        IP.SetActive(false);
-        MP.SetActive(false);
-        AP.SetActive(false);
-        LP.SetActive(false);
-
-        MB.GetComponent<Image>().color = InactiveColor;
-        IB.GetComponent<Image>().color = InactiveColor;
-        BB.GetComponent<Image>().color = ActiveColor;
-        AB.GetComponent<Image>().color = InactiveColor;
-        LB.GetComponent<Image>().color = InactiveColor;
+        DeactivatePanels(BP);
+        DeactivateButtons(BB);
     }
+
     public void Abil_button()
     {
-        AP.SetActive(true);
-        IP.SetActive(false);
-        BP.SetActive(false);
-        MP.SetActive(false);
-        LP.SetActive(false);
-
-        MB.GetComponent<Image>().color = InactiveColor;
-        IB.GetComponent<Image>().color = InactiveColor;
-        BB.GetComponent<Image>().color = InactiveColor;
-        AB.GetComponent<Image>().color = ActiveColor;
-        LB.GetComponent<Image>().color = InactiveColor;
+        DeactivatePanels(AP);
+        DeactivateButtons(AB);
     }
+
     public void Loot_button()
     {
-        LP.SetActive(true);
-        IP.SetActive(false);
-        BP.SetActive(false);
-        AP.SetActive(false);
-        MP.SetActive(false);
-
-        MB.GetComponent<Image>().color = InactiveColor;
-        IB.GetComponent<Image>().color = InactiveColor;
-        BB.GetComponent<Image>().color = InactiveColor;
-        AB.GetComponent<Image>().color = InactiveColor;
-        LB.GetComponent<Image>().color = ActiveColor;
+        DeactivatePanels(LP);
+        DeactivateButtons(LB);
     }
+
+    public void Training_Button()
+    {
+        TrainingPanel.SetActive(true);
+        ActivePanel.SetActive(false);
+        DisableButtons();
+    }
+    public void TrainingYes()
+    {
+        SceneManager.LoadSceneAsync("TrainingScene");
+    }
+    public void TrainingNo()
+    {
+        TrainingPanel.SetActive(false);
+        ActivePanel.SetActive(true);
+        EnableButtons();
+    }
+
     public void Ex_button()
     {
         SM.SetActive(true);
         gameObject.SetActive(false);
+    }
+
+    private void DeactivatePanels(GameObject activePanel)
+    {
+        LP.SetActive(false);
+        IP.SetActive(false);
+        BP.SetActive(false);
+        AP.SetActive(false);
+        MP.SetActive(false);
+        activePanel.SetActive(true);
+        ActivePanel = activePanel;
+    }
+
+    private void DeactivateButtons(GameObject activeButton)
+    {
+        MB.GetComponent<Image>().color = InactiveColor;
+        IB.GetComponent<Image>().color = InactiveColor;
+        BB.GetComponent<Image>().color = InactiveColor;
+        AB.GetComponent<Image>().color = InactiveColor;
+        LB.GetComponent<Image>().color = InactiveColor;
+        activeButton.GetComponent<Image>().color = ActiveColor;
+        ActiveButton = activeButton;
+    }
+
+    private void EnableButtons()
+    {
+        MB.SetActive(true);
+        IB.SetActive(true);
+        BB.SetActive(true);
+        AB.SetActive(true);
+        LB.SetActive(true);
+        TrainingButton.SetActive(true);
+        exitButton.SetActive(true);
+    }
+
+    private void DisableButtons()
+    {
+        MB.SetActive(false);
+        IB.SetActive(false);
+        BB.SetActive(false);
+        AB.SetActive(false);
+        LB.SetActive(false);
+        TrainingButton.SetActive(false);
+        exitButton.SetActive(false);
     }
 }
 
