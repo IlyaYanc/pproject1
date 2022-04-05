1214974d75a179cea8eae99ccb61b3a4dcd0f080
Ilya_Yan <ilyayan1755@gmail.com>
2022-04-05 19:51:07 +0300

    starting doing ads

---
+++ Project1Version9999/Assets/Scripts/UIScripts/ADs/LevelAdd.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelAdd : MonoBehaviour
+{
+    [SerializeField] private float adDuration;
+    [SerializeField] ItemBase[] dropItemsList;
+    [SerializeField] private Text a;
+
+    private Timer adDurationTimer;
+    private TimerManager timerManager;
+    void Start()
+    {
+        
+    }
+
+
+}
