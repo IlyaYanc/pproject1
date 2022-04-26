436a943454429c4744a42bb16d27a201e06f304e
Itclub21 <itclubunitylab21@gmail.com>
2022-04-26 20:04:26 +0300

    a
    b

---
+++ ghoul 7 += 1000/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //[SerializeField] GameObject cubeick;
+    [SerializeField] int speed;
+    Rigidbody rb;
+    bool right = true;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        speed = speed + 1;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            right = !right;
+        }
+        if (right)
+        {
+            rb.velocity = new Vector3(0f, 0f, speed);
+        }
+        else
+        {
+            rb.velocity = new Vector3(speed, 0f, 0f);
+        }
+    }
+}
