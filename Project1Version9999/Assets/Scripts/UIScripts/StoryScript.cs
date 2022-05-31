8f780ebb81e7beb811cdd606674e49920ea3104f
Ilya_Yan <ilyayan1755@gmail.com>
2022-05-31 12:09:11 +0300

    level1 add intro

--- Project1Version9999/Assets/Scripts/UIScripts/StoryScript.cs
+++ Project1Version9999/Assets/Scripts/UIScripts/StoryScript.cs
@@ -27,12 +27,13 @@ public class StoryScript : MonoBehaviour
 
     private void Start()
     {
+        StartCoroutine("INVisibleIE");
         StoryImageUI.sprite = StoryImage[0];
         StoryTextUI.text = StoryText[0];
         N++;
     }
     [Button]
-    private void NextStory()
+    public void NextStory()
     {
         StartCoroutine("VisibleIE");
     }
@@ -46,10 +47,13 @@ public class StoryScript : MonoBehaviour
             yield return new WaitForSeconds(0.05f);
         }
         if (N == StoryText.Length) gameObject.SetActive(false);
-        StoryImageUI.sprite = StoryImage[N];
-        StoryTextUI.text = StoryText[N];
-        N++;
-        StartCoroutine("INVisibleIE");
+        if(N != StoryText.Length)
+        {
+            StoryImageUI.sprite = StoryImage[N];
+            StoryTextUI.text = StoryText[N];
+            N++;
+            StartCoroutine("INVisibleIE");
+        }
     }
     IEnumerator INVisibleIE()
     {
