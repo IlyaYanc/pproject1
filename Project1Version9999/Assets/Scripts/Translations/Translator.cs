10ffb8ea22697a5e21da64a26dffe7f21e577851
Ilya_Yan <ilyayan1755@gmail.com>
2022-07-07 23:16:36 +0300

    training level translation
    только не для инвентаря

---
+++ Project1Version9999/Assets/Scripts/Translations/Translator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translator : MonoBehaviour
+{
+    [SerializeField] private LevelTextsContainer levelTextContainer;
+    [SerializeField] private LevelTranslation english;
+    void Start()
+    {
+        // выбрать язык
+        if (Application.systemLanguage != SystemLanguage.Russian)
+        {
+            Translate(english);
+        }
+    }
+
+    public void Translate(LevelTranslation language)
+    {
+        for(int i=0;i<levelTextContainer.levelTexts.Length;i++)
+        {
+            levelTextContainer.levelTexts[i].text = language.textTranslations[i];
+        }
+    }
+}
